5fe470c980af8424fe1bc1da0e7a3788c25c0f98
toptops <seongtab@naver.com>
2020-07-19 15:00:56 +0900

    [2020-07-19 배성탑] jpa 강의 -9

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 컬렉션 조회..
@@ -17,6 +24,10 @@ import java.util.List;
  * 1. 직접 조회
  *  - 직접 Lazy Loading 하기 위해 엔티티를 전부 한번식 호출했다.
  *  - ... 가장 안좋은 방법이다. 엔티티도 api에 노출됨.
+ *
+ * 2. DTO로 변환해서 조회
+ *  - DTO로 감싼다고 해서 엔티티를 노출해서는 안된다. (OrderItems 조차도 Dto로 전부 바꿔야한다.)
+ *  - 역시나.. 조회 쿼리가 각각 나가서 쿼리수가 많아진다... (Lazy loading)
  *
  */
 @RestController
@@ -39,4 +50,50 @@ public class OrderApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map( orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+
+    }
 }
