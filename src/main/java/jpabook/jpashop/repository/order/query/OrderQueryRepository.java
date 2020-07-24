ed71cda3f3006decfd6035eef304232b1ffb6677
toptops <seongtab@naver.com>
2020-07-24 13:51:30 +0900

    [2020-07-24 배성탑] jpa 강의 -12 - 틀린 소스 수정

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -71,11 +71,11 @@ public class OrderQueryRepository {
 
     public List<OrderFlatDto> findAllByDto_flat() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderStatus, o.status, d.address, i.name, oi.orderPrice, oi.count"
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)"
                             + " from Order o"
                             + " join o.member m"
                             + " join o.delivery d"
-                            + " join o.oderItems oi"
+                            + " join o.orderItems oi"
                             + " join oi.item i", OrderFlatDto.class)
                 .getResultList();
     }
